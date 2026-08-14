f925b1b33e0807918f4fb5dfdb49675fab8c1a27
AllenCodess <Aph296@protonmail.com>
2026-08-14 11:26:58 -0400

    created an outline for the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <Navbar />
-      <p> Test Job tracker</p>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router";
+
 const Navbar = () => {
-  return <h1> Navbar</h1>;
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="navbar-logo-container">
+          <h1>Job Tracker</h1>
+        </div>
+        <div className="navbar-items-container">
+          <li className="navbar-item">Home</li>
+          <li className="navbar-item">Login</li>
+          <li className="navbar-item">Create New Job</li>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
