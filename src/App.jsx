497a9588acab4531eea45e380f248907aa81022d
AllenCodess <Aph296@protonmail.com>
2026-08-14 10:47:39 -0400

    cleaned up the default and started with the nav componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+import "./index.css";
+
 function App() {
-  return <p> Test Job tracker</p>;
+  return (
+    <>
+      <Navbar />
+      <p> Test Job tracker</p>
+    </>
+  );
 }
 
 export default App;
