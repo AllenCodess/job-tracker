faad63c0f7b3431d6be5a1ded5d3cf6cbb9484f2
AllenCodess <Aph296@protonmail.com>
2026-08-14 16:11:16 -0400

    added 3 navigational pages to start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import JobPage from "./pages/Job";
 import "./index.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/jobs" element={<JobPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Job.jsx
@@ -0,0 +1,9 @@
+const JobPage = () => {
+  return (
+    <>
+      <h1>This is the Job Page</h1>
+    </>
+  );
+};
+
+export default JobPage;
