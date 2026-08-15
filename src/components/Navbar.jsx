187bd6939444704fb797477190939d7a1a29b0b3
AllenCodess <Aph296@protonmail.com>
2026-08-14 21:12:35 -0400

    rought draft of the hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="navbar-container">
         <div className="navbar-logo-container">
           <h1>Job Tracker</h1>
         </div>
-        <div className="navbar-items-container">
+        <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          ☰
+        </button>
+        <div className={`navbar-items-container ${isOpen ? "active" : ""} `}>
           <li className="navbar-item">
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={() => setIsOpen(false)}>
+              Home
+            </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/login">Login</Link>
+            <Link to="/login" onClick={() => setIsOpen(false)}>
+              Login
+            </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/jobs">Jobs</Link>
+            <Link to="/jobs" onClick={() => setIsOpen(false)}>
+              Jobs
+            </Link>
           </li>
         </div>
       </div>
