75da45c47f142830686f03aac8033d9708cb28e7
AllenCodess <Aph296@protonmail.com>
2026-08-15 10:32:11 -0400

    made changes to the hero section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,22 @@ const HomePage = () => {
     <>
       <div className="container">
         <div className="hero-section-container">
-          <div className="hero-left"></div>
-          <div className="hero-right"></div>
+          <div className="hero-left">
+            <h1 className="hero-text-header">Track you applications. Land your dream job.</h1>
+
+            <p className="hero-text-description">
+              Job Tracker helps users organize job applications, monitor prgress and stay consistent
+              on the path to get hired.
+            </p>
+            <button className="hero-btn">Login to get started.</button>
+          </div>
+          <div className="hero-right">
+            <img
+              className="hero-img"
+              src="./../src/assets/jobtracker-hero.png"
+              alt="preview image of the site"
+            />
+          </div>
         </div>
       </div>
     </>
