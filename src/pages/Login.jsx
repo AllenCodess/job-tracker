a245acf643037a8c3c04581c128b5b5745ddc913
AllenCodess <Aph296@protonmail.com>
2026-08-16 22:14:17 -0400

    started the skeleton for the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
 const LoginPage = () => {
   return (
     <>
-      <h1>Login </h1>
+      <div className="container">
+        <div className="login-form">
+          <div className="login-header">
+            <h1>Log into your account</h1>
+            <p className="login-description">
+              Welcome back! Please enter your details to continue.
+            </p>
+          </div>
+          <form className="login-inputs">
+            <label>Email address</label>
+            <input type="text" placeholder="Enter Email" className="email-input" />
+            <label>Password</label>
+            <input type="text" placeholder="Enter Password" className="password-input" />
+            <button className="login-btn">Login</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
