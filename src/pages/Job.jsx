d05d9c60a898f354ba2eedbb90d5ff67c11d265c
AllenCodess <Aph296@protonmail.com>
2026-08-18 11:14:22 -0400

    started on teh job details mapping through dummy data

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { jobs } from "../data/data";
 
 const JobPage = () => {
   return (
@@ -15,6 +16,27 @@ const JobPage = () => {
             Add Job
           </button>
         </div>
+      </div>
+      <div className="job-section container">
+        <ul>
+          {jobs.map((job) => (
+            <div className="single-job">
+              <div className="left-side">
+                <p className="job-position-title">{job.position}</p>
+                <div className="job-position-details-container">
+                  <li className="job-position-details">{job.company}</li>
+                  <li className="job-position-details">{job.location}</li>
+                  <li className="job-position-details">{job.workType}</li>
+                </div>
+              </div>
+              <div className="right-side">
+                <p className="job-position-details">{job.status}</p>
+                <p className="job-position-details">{job.appliedOn}</p>
+                <button>Details</button>
+              </div>
+            </div>
+          ))}
+        </ul>
       </div>
     </>
   );
