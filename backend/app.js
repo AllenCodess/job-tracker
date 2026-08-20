827a61a6902d8c616e6d4e689b556342edaf57f6
AllenCodess <Aph296@protonmail.com>
2026-08-20 12:05:09 -0400

    successfully created a db and connected to mongodb with mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config({ path: ".env" });
 
 const app = express();
+
+const DB = process.env.DATABASE;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error.message);
+  }
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.status(200).json({
