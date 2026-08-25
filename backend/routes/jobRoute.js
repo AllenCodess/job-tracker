ea87b0a277b5d07aedb1c82a4716f6f208a40c0c
AllenCodess <Aph296@protonmail.com>
2026-08-25 16:02:12 -0400

    added the route to job route

--- backend/routes/jobRoute.js
+++ backend/routes/jobRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protect } from "../controllers/authController.js";
-import { createJob, findJobs, findMyJobs } from "../controllers/jobController.js";
+import { createJob, findJobs, findMyJob, findMyJobs } from "../controllers/jobController.js";
 
 const router = express.Router();
 
 router.get("/", findJobs);
 router.post("/createjob", protect, createJob);
 router.get("/myjobs", protect, findMyJobs);
+router.get("/:id", protect, findMyJob);
 
 export default router;
